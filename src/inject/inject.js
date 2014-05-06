4a83fb813f037666e1ef904696cc09a00b91892f
Ben Jaffe <jaffe.ben@gmail.com>
2014-05-06 11:24:40 -0700

    refactor - now preserves current time of video

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -35,16 +35,57 @@ chrome.extension.sendMessage({}, function(response) {
 
 
         (function(){
-            var a = document.createElement('a');
-            var p = document.getElementById('player');
-            a.href = '#';
-            a.class = 'pluginFullscreenBtn';
-            a.appendChild(document.createTextNode('Full Screen'));
-            p.appendChild(a);
-            a.addEventListener('click', function(){
-                var urlBits = location.href.split('watch?v=');
-                location.href = urlBits[0] + 'embed/' + urlBits[1];
+            var player = document.getElementById('player');
+
+            // make a button for fullscreenification
+            var fullscreenBtn = document.createElement('a');
+            fullscreenBtn.href = '#';
+            fullscreenBtn.classList.add('pluginFullscreenBtn');
+            fullscreenBtn.appendChild(document.createTextNode('Full Screen'));
+
+            // when you click, make it happen
+            fullscreenBtn.addEventListener('click', function(){
+
+                // run code as if it's on the native page, since chrome extensions
+                // have limited access to page objects and methods
+                runAsPage(function(){
+
+                    // padNum adds leading zeros. It's in here so it's accessible in our code below.
+                    var padNum = function(num, digits) {
+                        var numStr = num.toString();
+                        while (numStr.length < digits) {
+                            numStr = '0' + numStr;
+                        }
+                        return numStr;
+                    };
+
+                    // get the current time of the video (only works when run in the page's memory space)
+                    var totalSeconds = document.getElementById('movie_player').getCurrentTime();
+
+                    // round to the nearest second
+                    totalSeconds = Math.floor(totalSeconds);
+
+                    // convert the time into youtube's format (eg. 132 is 1m 32s)
+                    var seconds = totalSeconds % 60;
+                    var totalMinutes = ((totalSeconds - seconds) / 60);
+                    var minutes = totalMinutes % 60;
+                    var totalHours = ((totalMinutes - minutes) / 60);
+                    var hours = totalHours % 60;
+
+                    var timeStr = padNum(hours, 2) + padNum(minutes, 2) + padNum(seconds, 2);
+                    console.log(timeStr);
+                    // chop up the url
+                    var urlBits = location.href.split('watch?v=');
+
+                    // put it back together
+                    location.href = urlBits[0] + 'embed/' + urlBits[1].split('#')[0] + '?start=' + timeStr + '&autoplay=1';
+                });
+
             }, false);
+
+            // add the button
+            player.appendChild(fullscreenBtn);
+
         })();
 
 	}
