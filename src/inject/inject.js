89d080482131a791ba5fbfac71488b2ae579c2ec
Ben Jaffe <jaffe.ben@gmail.com>
2014-05-06 11:21:41 -0700

    runAsPage allows us to run code within the page's memory space

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -6,6 +6,34 @@ chrome.extension.sendMessage({}, function(response) {
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
 		// ----------------------------------------------------------
+
+        /**
+         * This function runs the function parameter within the context of the actual page,
+         * since chrome extensions have limited access to page objects and methods.
+         * This only works for functions that are self-contained - ie, closures don't persist
+         * when you stringify functions like this, so don't rely on anything outside of the
+         * function itself.
+         */
+        var runAsPage = function(fun) {
+            // only run functions
+            if (!typeof fun === 'function') return false;
+
+            // make a new script element
+            var script = document.createElement('script');
+
+            // stringify function and invoke in string
+            var scriptContent = '(' + fun.toString() + ')();';
+
+            // add script content to the script, and add the script to the body, "running it"
+            script.appendChild(document.createTextNode(scriptContent));
+            document.body.appendChild(script);
+
+            // success
+            return true;
+        };
+
+
+
         (function(){
             var a = document.createElement('a');
             var p = document.getElementById('player');
