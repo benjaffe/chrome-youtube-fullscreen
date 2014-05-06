e66d89c87f75f4fd0f050f6acd28dc7e9cd1639f
Ben Jaffe <jaffe.ben@gmail.com>
2014-05-06 09:35:54 -0700

    init

---
+++ src/inject/inject.js
@@ -0,0 +1,30 @@
+chrome.extension.sendMessage({}, function(response) {
+	var readyStateCheckInterval = setInterval(function() {
+	if (document.readyState === "complete") {
+		clearInterval(readyStateCheckInterval);
+
+		// ----------------------------------------------------------
+		// This part of the script triggers when page is done loading
+		// ----------------------------------------------------------
+        (function(){
+            var a = document.createElement('a');
+            var p = document.getElementById('player');
+            a.href = '#';
+            a.class = 'pluginFullscreenBtn';
+            a.appendChild(document.createTextNode('Full Screen'));
+            p.style.position = 'relative';
+            a.style.position = 'absolute';
+            a.style.bottom = '8px';
+            a.style.zIndex = '100000';
+            a.style.right = '0';
+            a.style.marginRight = '120px';
+            p.appendChild(a);
+            a.addEventListener('click', function(){
+                var urlBits = location.href.split('watch?v=');
+                location.href = urlBits[0] + 'embed/' + urlBits[1];
+            }, false);
+        })();
+
+	}
+	}, 10);
+});
