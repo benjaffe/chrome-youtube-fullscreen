f3ed1508b0611b59b8acb4d44a21cbf00f74b738
Ben Jaffe <jaffe.ben@gmail.com>
2014-05-07 08:49:20 -0700

    renamed to standalonify, moved standalonification into its own function

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,6 +7,7 @@ chrome.extension.sendMessage({}, function(response) {
 		// This part of the script triggers when page is done loading
 		// ----------------------------------------------------------
 
+
         /**
          * This function runs the function parameter within the context of the actual page,
          * since chrome extensions have limited access to page objects and methods.
@@ -32,59 +33,63 @@ chrome.extension.sendMessage({}, function(response) {
             return true;
         };
 
+        /**
+         * This function makes the standalonification happen!
+         */
+        var standalonify = function(){
+
+            // run code as if it's on the native page, since chrome extensions
+            // have limited access to page objects and methods
+            runAsPage(function(){
+
+                // padNum adds leading zeros. It's in here so it's accessible in our code below.
+                var padNum = function(num, digits) {
+                    var numStr = num.toString();
+                    while (numStr.length < digits) {
+                        numStr = '0' + numStr;
+                    }
+                    return numStr;
+                };
+
+                // get the current time of the video (only works when run in the page's memory space)
+                var totalSeconds = document.getElementById('movie_player').getCurrentTime();
+
+                // round to the nearest second
+                totalSeconds = Math.floor(totalSeconds);
+
+                // convert the time into youtube's format (eg. 132 is 1m 32s)
+                var seconds = totalSeconds % 60;
+                var totalMinutes = ((totalSeconds - seconds) / 60);
+                var minutes = totalMinutes % 60;
+                var totalHours = ((totalMinutes - minutes) / 60);
+                var hours = totalHours % 60;
+
+                var timeStr = padNum(hours, 2) + padNum(minutes, 2) + padNum(seconds, 2);
+                console.log(timeStr);
+                // chop up the url
+                var urlBits = location.href.split('watch?v=');
+
+                // put it back together
+                location.href = urlBits[0] + 'embed/' + urlBits[1].split('#')[0] + '?start=' + timeStr + '&autoplay=1';
+            });
+        };
+
 
 
         (function(){
             var player = document.querySelector('.html5-player-chrome');
 
-            // make a button for fullscreenification
-            var fullscreenBtn = document.createElement('a');
-            fullscreenBtn.href = '#';
-            fullscreenBtn.className = 'ytp-button ytp-button-standalone';
-            fullscreenBtn.appendChild(document.createTextNode('Standalone'));
+            // make a button for standalonification
+            var standalonifyBtn = document.createElement('a');
+            standalonifyBtn.href = '#';
+            standalonifyBtn.className = 'ytp-button ytp-button-standalone';
+            standalonifyBtn.appendChild(document.createTextNode('Standalone'));
 
             // when you click, make it happen
-            fullscreenBtn.addEventListener('click', function(){
-
-                // run code as if it's on the native page, since chrome extensions
-                // have limited access to page objects and methods
-                runAsPage(function(){
-
-                    // padNum adds leading zeros. It's in here so it's accessible in our code below.
-                    var padNum = function(num, digits) {
-                        var numStr = num.toString();
-                        while (numStr.length < digits) {
-                            numStr = '0' + numStr;
-                        }
-                        return numStr;
-                    };
-
-                    // get the current time of the video (only works when run in the page's memory space)
-                    var totalSeconds = document.getElementById('movie_player').getCurrentTime();
-
-                    // round to the nearest second
-                    totalSeconds = Math.floor(totalSeconds);
-
-                    // convert the time into youtube's format (eg. 132 is 1m 32s)
-                    var seconds = totalSeconds % 60;
-                    var totalMinutes = ((totalSeconds - seconds) / 60);
-                    var minutes = totalMinutes % 60;
-                    var totalHours = ((totalMinutes - minutes) / 60);
-                    var hours = totalHours % 60;
-
-                    var timeStr = padNum(hours, 2) + padNum(minutes, 2) + padNum(seconds, 2);
-                    console.log(timeStr);
-                    // chop up the url
-                    var urlBits = location.href.split('watch?v=');
-
-                    // put it back together
-                    location.href = urlBits[0] + 'embed/' + urlBits[1].split('#')[0] + '?start=' + timeStr + '&autoplay=1';
-                });
-
-            }, false);
+            standalonifyBtn.addEventListener('click', standalonify, false);
 
             // add the button
-            player.appendChild(fullscreenBtn);
+            player.appendChild(standalonifyBtn);
 
         })();
 
