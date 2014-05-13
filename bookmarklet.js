fa982f321d1aff2dff7a320eea561647f6bfe31d
Ben Jaffe <jaffe.ben@gmail.com>
2014-05-13 09:28:13 -0700

    updated bookmarklet for 0.0.4

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,45 +1,39 @@
 // minified via http://javascriptcompressor.com/
 (function(){
-    // padNum adds leading zeros. It's in here so it's accessible in our code below.
-    var padNum = function(num, digits) {
-        var numStr = num.toString();
-        while (numStr.length < digits) {
-            numStr = '0' + numStr;
-        }
-        return numStr;
-    };
+    var timeStr, totalSeconds;//, seconds, minutes, totalMinutes, hours, totalHours;
+    var location = window.location;
+    var urlBits, videoIdParameter, videoId, newUrl;
 
     // get the current time of the video (only works when run in the page's memory space)
-    var totalSeconds = document.getElementById('movie_player').getCurrentTime();
+    totalSeconds = document.getElementById('movie_player').getCurrentTime();
 
     // round to the nearest second
     totalSeconds = Math.floor(totalSeconds);
 
     // convert the time into youtube's format (eg. 132 is 1m 32s)
-    var seconds = totalSeconds % 60;
-    var totalMinutes = ((totalSeconds - seconds) / 60);
-    var minutes = totalMinutes % 60;
-    var totalHours = ((totalMinutes - minutes) / 60);
-    var hours = totalHours % 60;
+    // seconds = totalSeconds % 60;
+    // totalMinutes = ((totalSeconds - seconds) / 60);
+    // minutes = totalMinutes % 60;
+    // totalHours = ((totalMinutes - minutes) / 60);
+    // hours = totalHours % 60;
 
-    var timeStr = padNum(hours, 2) + padNum(minutes, 2) + padNum(seconds, 2);
-    console.log(timeStr);
+    // timeStr = padNum(hours, 2) + padNum(minutes, 2) + padNum(seconds, 2);
+    // console.log(timeStr);
+    timeStr = String(totalSeconds);
 
     // reform the url
-    var location = window.location;
-
-    var urlBits = location.search.slice(1).split('&');
+    urlBits = location.search.slice(1).split('&');
     urlBits.push( ('start=' + timeStr), ('autoplay=1') );
 
     // find the v url parameter (the video id)
     for (i=0; urlBits[i].split('=')[0] !== 'v'; i++) {}
 
     // get the video id while removing it from the array of parameters
-    var videoIdParameter = urlBits.splice(i,1)[0];
-    var videoId = videoIdParameter.split('v=')[1];
+    videoIdParameter = urlBits.splice(i,1)[0];
+    videoId = videoIdParameter.split('v=')[1];
 
     // form the new url
-    var newUrl = location.origin + '/embed/' + videoId + '?' + urlBits.join('&') + location.hash;
+    newUrl = location.origin + '/embed/' + videoId + '?' + urlBits.join('&') + location.hash;
 
     // GO!
     location.href = newUrl;
